c01bfbe62d213e937ef0d224e704e12ce8e3db82
granddaifuku <>
2021-01-09 16:30:18 +0900

    feat: スコア計算の実装

--- src/scoring.rs
+++ src/scoring.rs
@@ -3,6 +3,7 @@ use super::{
     error::Error,
     models::{Submits, TestcaseSets, TestcaseTestcaseSets},
 };
+use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 
 #[allow(dead_code)]
@@ -15,35 +16,54 @@ pub async fn scoring(submit: Submits) -> Result<i64, Error> {
     let testcase_sets: Vec<TestcaseSets> = sqlx::query_as(
         r#"
 SELECT id, points FROM testcase_sets
-WHERE deleted_at IS NULL AND problem_id = $1
-"#,
-    )
-    .bind(submit.problem_id)
-    .fetch_all(&pool)
-    .await?;
-    let testcase_testcase_sets: Vec<TestcaseTestcaseSets> = sqlx::query_as(
-        r#"
-SELECT testcase_id, testcase_set_id FROM testcase_testcase_sets
-INNER JOIN testcases ON testcase_testcase_sets.testcase_id = testcases.id
-WHERE problem_id = $1 AND testcase_testcase_sets.deleted IS NULL AND testcases.deleted IS NULL
+WHERE deleted_at IS NULL AND problem_id = ?
 "#,
     )
     .bind(submit.problem_id)
     .fetch_all(&pool)
     .await?;
 
-    let mut testcase_set_map: HashMap<i64, Vec<TestcaseTestcaseSets>> = HashMap::new();
-    for test_case_set in &testcase_sets {
-        testcase_set_map.insert(test_case_set.id, Vec::new());
+    let testcase_testcase_sets: Vec<TestcaseTestcaseSets> = sqlx::query_as(
+        r#"
+SELECT testcase_id, testcase_set_id FROM testcase_testcase_sets
+INNER JOIN testcases ON testcase_testcase_sets.testcase_id = testcases.id
+WHERE problem_id = ? AND testcase_testcase_sets.deleted IS NULL AND testcases.deleted_at IS NULL
+"#,
+    )
+    .bind(submit.problem_id)
+    .fetch_all(&pool)
+    .await?;
+
+    let mut testcase_set_map: HashMap<i64, Vec<i64>> = HashMap::new();
+    for testcase_testcase_set in &testcase_testcase_sets {
+        match testcase_set_map.entry(testcase_testcase_set.testcase_set_id) {
+            Entry::Vacant(e) => {
+                e.insert(vec![testcase_testcase_set.testcase_id]);
+            }
+            Entry::Occupied(mut e) => {
+                e.get_mut().push(testcase_testcase_set.testcase_id);
+            }
+        }
     }
-    for testcase_testcase_set in testcase_testcase_sets {}
 
     let mut score: i64 = 0;
     for testcase_set in &testcase_sets {
         #[allow(non_snake_case)]
         #[allow(unused_mut)]
         let mut is_AC = true;
-        for testcase_id in &testcase_set_map[&testcase_set.id] {}
+        for testcase_id in &testcase_set_map[&testcase_set.id] {
+            let result_status: (String,) = sqlx::query_as(
+                "SELECT status FROM testcase_results
+WHERE submit_id = ?",
+            )
+            .bind(submit.id)
+            .fetch_one(&pool)
+            .await?;
+            if result_status.0 != "AC".to_string() {
+                is_AC = false;
+                break;
+            }
+        }
         if is_AC {
             let point: i64 = From::from(testcase_set.point);
             score += point;
