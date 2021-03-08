a7ac2f5bfb704c7d8c101b640de59dc8aa6f9c80
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-03-09 06:25:15 +0900

    --features db_test の場合のみDBのtest を実行

--- src/db.rs
+++ src/db.rs
@@ -8,41 +8,43 @@ pub async fn new_pool(config: &Config) -> Result<DbPool> {
     Ok(pool)
 }
 
-#[tokio::test]
-async fn test_db() -> Result<()> {
-    use crate::repository::SubmitRepository;
-    let config = Config {
-        database_url: "mysql://root:root@localhost:3306/p6jav_cafecoder_prod".to_string(),
-    };
-    let pool = new_pool(&config).await?;
-    let result = pool.begin().await?.get_submits().await?;
-    dbg!(result);
+#[cfg(feature = "db_test")]
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[tokio::test]
+    async fn test_db() -> Result<()> {
+        use crate::config::load_config;
+        use crate::repository::SubmitRepository;
+        let config = load_config()?;
+        let pool = new_pool(&config).await?;
+        let result = pool.begin().await?.get_submits().await?;
+        dbg!(result);
 
-    Ok(())
-}
+        Ok(())
+    }
 
-#[tokio::test]
-async fn test_db2() -> Result<()> {
-    use crate::repository::ProblemsRepository;
-    let config = Config {
-        database_url: "mysql://root:root@localhost:3306/p6jav_cafecoder_prod".to_string(),
-    };
-    let pool = new_pool(&config).await?;
-    let result = pool.fetch_problem(1).await?;
-    dbg!(result);
+    #[tokio::test]
+    async fn test_db2() -> Result<()> {
+        use crate::config::load_config;
+        use crate::repository::ProblemsRepository;
+        let config = load_config()?;
+        let pool = new_pool(&config).await?;
+        let result = pool.fetch_problem(1).await?;
+        dbg!(result);
 
-    Ok(())
-}
+        Ok(())
+    }
 
-#[tokio::test]
-async fn test_db3() -> Result<()> {
-    use crate::repository::TestcasesRepository;
-    let config = Config {
-        database_url: "mysql://root:root@localhost:3306/p6jav_cafecoder_prod".to_string(),
-    };
-    let pool = new_pool(&config).await?;
-    let result = pool.fetch_testcases(1).await?;
-    dbg!(result);
+    #[tokio::test]
+    async fn test_db3() -> Result<()> {
+        use crate::config::load_config;
+        use crate::repository::TestcasesRepository;
+        let config = load_config()?;
+        let pool = new_pool(&config).await?;
+        let result = pool.fetch_testcases(1).await?;
+        dbg!(result);
 
-    Ok(())
+        Ok(())
+    }
 }
