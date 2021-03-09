0d199440358491b93e8c99da765674d4883c4deb
uesugi6111 <59960488+aburaya6111@users.noreply.github.com>
2021-03-10 08:01:13 +0900

    タイムアウトを180秒へ暫定設定

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,13 @@ mod repository;
 mod task;
 mod utils;
 use anyhow::Result;
+use core::time::Duration;
 use futures::future::join_all;
 use std::sync::Arc;
 
 // TODO(magurotuna): ここの値も要検討
 const JOB_THREADS: usize = 3;
+const HTTP_TIMEOUT: u64 = 180;
 
 // TODO(magurotuna): スレッド数指定を柔軟に行うため、Tokio の RuntimeBuilder を使うよう書き換える
 #[tokio::main(worker_threads = 4)]
@@ -19,7 +21,9 @@ async fn main() -> Result<()> {
     let config = config::load_config()?;
     let db_conn = Arc::new(db::new_pool(&config).await?);
     let docker_conn = Arc::new(bollard::Docker::connect_with_http_defaults()?);
-    let http_client = reqwest::Client::new();
+    let http_client = reqwest::Client::builder()
+        .timeout(Duration::new(HTTP_TIMEOUT, 0))
+        .build()?;
 
     let mut handles = Vec::new();
     for _ in 0..JOB_THREADS {
