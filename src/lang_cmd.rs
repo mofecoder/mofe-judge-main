49b9b91902a8587c666dd524ba4545f8680d4ecf
kichi2004 <mail@kichi2004.jp>
2023-07-27 20:54:14 +0900

    言語アップデートに対応

--- src/lang_cmd.rs
+++ src/lang_cmd.rs
@@ -10,196 +10,178 @@ pub struct Command {
     pub file_name: String,
 }
 
+impl Command {
+    fn new(compile: &str, run: &str, file_name: &str) -> Self {
+        Self {
+            compile: compile.to_string() + " 2> /judge/userStderr.txt",
+            run: run.to_string() + " > /judge/userStdout.txt 2> /judge/userStderr.txt",
+            file_name: file_name.to_string(),
+        }
+    }
+}
+
 pub fn generate_lang_cmd_map() -> HashMap<String, Command> {
     let mut map = HashMap::new();
+    // $INSERT_HERE$
+    map.insert(
+        "c17_gcc:12.2.0".to_string(),
+        Command::new(
+            "gcc-12 ./Main.c -O2 -lm -std=gnu17 -o Main.out -DONLINE_JUDGE -Wall -Wextra",
+            "./Main.out",
+            "Main.c"
+        )
+    );
     map.insert(
         "cpp20_gcc:12.2.0".to_string(),
-        Command {
-            compile: "g++-12 /judge/Main.cpp -O2 -lm -std=gnu++17 -o /judge/Main.out 2> /judge/userStderr.txt"
-                .to_string(),
-            run:
-            "/judge/Main.out < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt"
-                .to_string(),
-            file_name: "Main.cpp".to_string(),
-        },
+        Command::new(
+            "g++-12 ./Main.cpp -O2 -lm -std=gnu++20 -I/opt/ac-library -o Main.out -DONLINE_JUDGE -Wall -Wextra",
+            "./Main.out",
+            "Main.cpp"
+        )
     );
     map.insert(
-        "python:3.11.4".to_string(), //python3
-        Command {
-            compile: "python3 -m py_compile /judge/Main.py 2> /judge/userStderr.txt".to_string(),
-            run: "python3 /judge/Main.py < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.py".to_string(),
-        },
+        "cpp23_gcc:12.2.0".to_string(),
+        Command::new(
+            "g++-12 ./Main.cpp -O2 -lm -std=gnu++2b -I/opt/ac-library -o Main.out -DONLINE_JUDGE -Wall -Wextra",
+            "./Main.out",
+            "Main.cpp"
+        )
     );
     map.insert(
-        "c17_gcc:10.2.0".to_string(), //C17
-        Command {
-            compile: "gcc-10 /judge/Main.c -O2 -lm -std=gnu17 -o /judge/Main.out 2> /judge/userStderr.txt"
-                .to_string(),
-            run:
-                "/judge/Main.out < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt"
-                    .to_string(),
-            file_name: "Main.c".to_string(),
-        },
+        "java:17.0.7".to_string(),
+        Command::new(
+            "javac ./Main.java",
+            "java ./Main",
+            "Main.java"
+        )
     );
     map.insert(
-        "cpp17_gcc:10.2.0".to_string(), //C++17
-        Command {
-            compile: "g++-10 /judge/Main.cpp -O2 -lm -std=gnu++17 -o /judge/Main.out 2> /judge/userStderr.txt"
-                .to_string(),
-            run:
-                "/judge/Main.out < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt"
-                    .to_string(),
-            file_name: "Main.cpp".to_string(),
-        },
+        "python:3.11.4".to_string(),
+        Command::new(
+            "python3.11 -m py_compile ./Main.py && python3.11 ./Main.py 2> /dev/null",
+            "python3.11 ./Main.py",
+            "Main.py"
+        )
     );
     map.insert(
-        "cpp17-acl_gcc:10.2.0".to_string(), //C++17 + ACL
-        Command {
-            compile:
-                "g++-10 /judge/Main.cpp -O2 -lm -std=gnu++17 -I/ -o /judge/Main.out 2> /judge/userStderr.txt"
-                    .to_string(),
-            run:
-                "/judge/Main.out < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt"
-                    .to_string(),
-            file_name: "Main.cpp".to_string(),
-        },
+        "".to_string(),
+        Command::new(
+            "mcs ./Main.cs -out:Main.exe",
+            "python3.11 ./Main.py",
+            "Main.cs"
+        )
     );
     map.insert(
-        "cpp20_gcc:10.2.0".to_string(), //C++20
-        Command {
-            compile: "g++-10 /judge/Main.cpp -O2 -lm -std=gnu++2a -o /judge/Main.out 2> /judge/userStderr.txt"
-                .to_string(),
-            run:
-                "/judge/Main.out < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt"
-                    .to_string(),
-            file_name: "Main.cpp".to_string(),
-        },
+        "go:1.20.6".to_string(),
+        Command::new(
+            "go build ./Main.go -o Main.out",
+            "./Main.out",
+            "Main.go"
+        )
     );
     map.insert(
-        "java:11.0.9".to_string(), //java11
-        Command {
-            compile: "javac -encoding UTF-8 /judge/Main.java 2> /judge/userStderr.txt".to_string(),
-            run: "cd /judge && java Main < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt"
-                .to_string(),
-            file_name: "Main.java".to_string(),
-        },
+        "cs_dotnet:7.0".to_string(),
+        Command::new(
+            "dotnet publish -o . -c Release -v q --nolog 1>&2",
+            "./Main",
+            "Main.cs"
+        )
     );
     map.insert(
-        "python:3.9.0".to_string(), //python3
-        Command {
-            compile: "python3.9 -m py_compile /judge/Main.py 2> /judge/userStderr.txt".to_string(),
-            run: "python3.9 /judge/Main.py < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.py".to_string(),
-        },
+        "nim:1.6.14".to_string(),
+        Command::new(
+            "nim cpp -d:release --opt:speed --multimethods:on -o:Main.out Main.nim",
+            "./Main.out",
+            "Main.nim"
+        )
     );
     map.insert(
-        "pypy3:7.3.3".to_string(), //pypy3
-        Command {
-            compile: "pypy3 -m py_compile /judge/Main.py 2> /judge/userStderr.txt".to_string(),
-            run: "pypy3 /judge/Main.py < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.py".to_string(),
-        },
-    );
-    map.insert( "cs_mono:6.12.0.90".to_string(), //C#
-        Command {
-            compile: "source ~/.profile && mcs /judge/Main.cs -out:/judge/Main.exe 2> /judge/userStderr.txt".to_string(),
-            run: "source ~/.profile && mono /judge/Main.exe < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.cs".to_string(),
-        },
-    );
-    map.insert( "cs_dotnet:5.0".to_string(), // C#
-        Command {
-            compile: "source ~/.profile && export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1 && cd /judge/Main &&  dotnet new console && mv /judge/Main.cs /judge/Main/Program.cs && dotnet publish -c Release --nologo -v q -o . 2> /judge/userStderr.txt && cd /".to_string(),
-            run: "source ~/.profile && dotnet /judge/Main/Main.dll < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.cs".to_string(),
-        },
-    );
-    map.insert( "go:1.15.5".to_string(), //golang
-        Command {
-            compile: "source ~/.profile && mv /judge/Main.go /judge/Main && cd /judge/Main && go build -o Main.out Main.go 2> /judge/userStderr.txt".to_string(),
-            run: "/judge/Main/Main.out < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.go".to_string(),
-        },
-    );
-    map.insert( "nim:1.4.0".to_string(),
-        Command {
-            compile: "source ~/.profile && nim cpp -d:release --opt:speed --multimethods:on -o:/judge/Main.out /judge/Main.nim 2> /judge/userStderr.txt".to_string(),
-            run: "/judge/Main.out < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.nim".to_string(),
-        },
-    );
-    map.insert( "rust:1.48.0".to_string(),
-        Command {
-            compile: "source ~/.profile && cd /judge/rust_workspace && mv /judge/Main.rs /judge/rust_workspace/src/main.rs && cargo build --release 2> /judge/userStderr.txt && cd /".to_string(),
-            run: "/judge/rust_workspace/target/release/Rust < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.rs".to_string(),
-        },
-    );
-    map.insert( "ruby:2.7.2".to_string(),
-        Command {
-            compile: "source ~/.profile && ruby -w -c /judge/Main.rb 2> /judge/userStderr.txt".to_string(),
-            run: "source ~/.profile && ruby /judge/Main.rb < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.rb".to_string(),
-        },
-    );
-    map.insert( "kotlin:1.4.10".to_string(),
-        Command {
-            compile: "source ~/.profile && kotlinc /judge/Main.kt -include-runtime -d Main.jar 2> /judge/userStderr.txt".to_string(),
-            run: "source ~/.profile && kotlin /judge/Main.jar < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.kt".to_string(),
-        },
+        "rust:1.71.0".to_string(),
+        Command::new(
+            "cargo build --release",
+            "./Main.out",
+            "Main.rs"
+        )
     );
     map.insert(
-        "fortran:10.2.0".to_string(),
-        Command {
-            compile: "gfortran-10 -O2 /judge/Main.f90 -o /judge/Main.out 2> /judge/userStderr.txt".to_string(),
-            run:
-                "/judge/Main.out < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt"
-                    .to_string(),
-            file_name: "Main.f90".to_string(),
-        },
+        "ruby:3.2.2".to_string(),
+        Command::new(
+            "ruby -w -c ./Main.rb",
+            "ruby ./Main.rb",
+            "Main.rb"
+        )
     );
     map.insert(
-        "perl:5.30.0".to_string(),
-        Command {
-            compile: "perl -c /judge/Main.pl 2> /judge/userStderr.txt".to_string(),
-            run: "perl /judge/Main.pl < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.pl".to_string(),
-        },
-    );
-    map.insert( "raku:2020.10".to_string(),
-        Command {
-            compile: "source ~/.profile && perl6 -c /judge/Main.p6 2> /judge/userStderr.txt".to_string(),
-            run: "source ~/.profile && perl6 /judge/Main.p6 < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.p6".to_string(),
-        },
+        "kotlin:1.9.0".to_string(),
+        Command::new(
+            "kotlinc ./Main.kt -include-runtime -d Main.jar",
+            "kotlin Main.jar",
+            "Main.kt"
+        )
     );
     map.insert(
-        "crystal:0.35.1".to_string(),
-        Command {
-            compile: "crystal build /judge/Main.cr -o /judge/Main.out 2> /judge/userStderr.txt".to_string(),
-            run:
-                "/judge/Main.out < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt"
-                    .to_string(),
-            file_name: "Main.cr".to_string(),
-        },
+        "fortran:12.2.0".to_string(),
+        Command::new(
+            "gfortran -O2 Main.f90 -o Main.out",
+            "./Main.out",
+            "Main.f90"
+        )
     );
     map.insert(
-        "text_cat:8.30".to_string(),
-        Command {
-            compile: ":".to_string(),
-            run: "cat /judge/Main.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.txt".to_string(),
-        },
+        "crystal:1.8.2".to_string(),
+        Command::new(
+            "crystal build --release --no-debug --no-color -o Main.out ./Main.cr",
+            "./Main.out",
+            "Main.cr"
+        )
     );
     map.insert(
-        "bash:5.0.17".to_string(),
-        Command {
-            compile: "bash -n /judge/Main.sh 2> /judge/userStderr.txt".to_string(),
-            run: "bash /judge/Main.sh < /judge/testcase.txt > /judge/userStdout.txt 2> /judge/userStderr.txt".to_string(),
-            file_name: "Main.sh".to_string(),
-        },
+        "text_cat:8.32".to_string(),
+        Command::new(
+            ":",
+            "cat Main.txt",
+            "Main.txt"
+        )
+    );
+    map.insert(
+        "perl:5.36.0".to_string(),
+        Command::new(
+            "perl -W -c ./Main.pl",
+            "perl -X ./Main.pl",
+            "Main.pl"
+        )
+    );
+    map.insert(
+        "raku:2022.12".to_string(),
+        Command::new(
+            "perl6 -c ./Main.p6",
+            "perl6 ./Main.p6",
+            "Main.p6"
+        )
+    );
+    map.insert(
+        "bash:5.2.15".to_string(),
+        Command::new(
+            "bash -n ./Main.sh",
+            "bash ./Main.sh",
+            "Main.sh"
+        )
+    );
+    map.insert(
+        "pypy310:7.3.12".to_string(),
+        Command::new(
+            "pypy3 -m py_compile ./Main.py",
+            "pypy3 ./Main.py",
+            "Main.py"
+        )
+    );
+    map.insert(
+        "dc:1.4.1".to_string(),
+        Command::new(
+            ":",
+            "dc Main.dc",
+            "Main.dc"
+        )
     );
     map
 }
